8e9cbce857b0b94af733596abd7e1b72c3bb5509
Alexisss11 <alexispurple1978@gmail.com>
2025-11-14 20:58:23 +0000

    practice

--- week 13  csp/3 — Lists and List Functions.py
+++ week 13  csp/3 — Lists and List Functions.py
@@ -12,21 +12,49 @@
 # instead of storing a bunch of variables you can stopre them in a list
 # This makes our job easier when we need to manage multiples items 
 # performance task answer
-my_list1= ['1', '2', '3', '4', '5']
-print(my_list1)
-print(my_list1[0])
-print(my_list1[0:4])
-print(my_list1)
-print(my_list1[-1])
-# Examples:
-my_list1.extend([10,11,12,13,14])
-print(my_list1)
-# add 500 more numbers to the list
-my_list1.extend(list(range(15, 515)))
-print(my_list1)
-my_list1.extend(list(range(515, 1115)))
-print(my_list1)
+# my_list1= ['1', '2', '3', '4', '5']
+# print(my_list1)
+# print(my_list1[0])
+# print(my_list1[0:4])
+# print(my_list1)
+# print(my_list1[-1])
+# # Examples:
+# my_list1.extend([10,11,12,13,14])
+# print(my_list1)
+# # add 500 more numbers to the list
+# my_list1.extend(list(range(15, 515)))
+# print(my_list1)
+# my_list1.extend(list(range(515, 1115)))
+# print(my_list1)
 
+new_list = ['a', 'b', 'c']
+print(new_list)
+new_list.append ('d')
+print(new_list)
+#removiong an item from the list
+removed_item = new_list.pop()
+#re,pves the last 
+#sorting the list
+numbers = [ 4,2,5,1,3]
+numbers.sort()
+print(numbers)
+#rewversing the list
+numbers.reverse()
+print(numbers)
+#inserting an item at a specific positoon
+numbers.insert(2, 10)
+print(numbers)
+third_list = [7,8, 9]
+third_list[0] = 6
+print(third_list)
+third_list[-1] = 10
+print(third_list)
+
+import random
+random_list = random.sample(range(1,1000), 100)
+# this will create a list of 10 unique randome numbers between 1-99
+print(random_list)
+print(sorted(random_list))
 my_list = ['apple', 'banana', 'cherry']
 print(my_list[0])         # apple
 print(my_list[1:])        # ['banana', 'cherry']
@@ -40,6 +68,7 @@ print(my_list)
 numbers = [3, 1, 4, 2]
 numbers.sort()
 print(numbers)
+
 
 
 # Practice Problems:
