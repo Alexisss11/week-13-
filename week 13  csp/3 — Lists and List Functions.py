98710455237c3b9783296037d277f4543ab9016d
Alexisss11 <alexispurple1978@gmail.com>
2025-11-14 20:38:40 +0000

    practice

--- week 13  csp/3 — Lists and List Functions.py
+++ week 13  csp/3 — Lists and List Functions.py
@@ -3,8 +3,29 @@
 
 # Topics Covered:
 # Creating lists, indexing, slicing, appending, popping, sorting, reversing.
-
+#collection = single "variable" used to store multiple values
+#list = [] ordere4d and changeable, dupllicates are OK
+# set = {} unordered and immutable, but Add/remove OK, No duplicates
+# Tuple = () ordered and unchangable. Duplicates OK FASTER
+#list are ordered collections of items
+# list are 
+# instead of storing a bunch of variables you can stopre them in a list
+# This makes our job easier when we need to manage multiples items 
+# performance task answer
+my_list1= ['1', '2', '3', '4', '5']
+print(my_list1)
+print(my_list1[0])
+print(my_list1[0:4])
+print(my_list1)
+print(my_list1[-1])
 # Examples:
+my_list1.extend([10,11,12,13,14])
+print(my_list1)
+# add 500 more numbers to the list
+my_list1.extend(list(range(15, 515)))
+print(my_list1)
+my_list1.extend(list(range(515, 1115)))
+print(my_list1)
 
 my_list = ['apple', 'banana', 'cherry']
 print(my_list[0])         # apple
@@ -24,9 +45,9 @@ print(numbers)
 # Practice Problems:
 
 # Create a list with 5 of your favorite foods.
-
+list2 = ['cookies', 'coffee', 'beans', 'steak', 'karissa']
 # Print the second and last item.
-
+print(list2[3:])
 # Add a new item using .append().
 
 # Remove the first item using .pop(0).
